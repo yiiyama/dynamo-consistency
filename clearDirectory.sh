0e676a59765dcbfcc484e6ce9ee83aea33736e89
dabercro <daniel.r.abercrombie@gmail.com>
2014-10-28 15:59:29 -0400

    Clearing old directories.

--- clearDirectory.sh
+++ clearDirectory.sh
@@ -7,6 +7,12 @@ dir=$1
 returnTo=`pwd`
 
 cd $dir                        # Go to specified directory
+echo 'Removing directory:'
+echo '**************************************************************************************'
+echo `pwd`
+echo '**************************************************************************************'
+echo 'Hit Ctrl-C to cancel.'
+read -t 5 -p 'Hit enter or wait 5 seconds to continue.'
 
 for term in `ls -A`; do        # Get everything in that directory
     if [ -f $term ]; then      # Remove everything that is a file
@@ -14,14 +20,13 @@ for term in `ls -A`; do        # Get everything in that directory
     fi
 done
 
-empty=true
-while [ $empty ]; do           # Continue this until directory is not empty
+while true; do                 # Continue this until directory is not empty
     if [ ! "$(ls -A)" ]; then  # If empty, cd out and remove directory
         dirToRemove=`pwd`
         cd ..
         rmdir $dirToRemove
     else                       # If not empty, stop the loop
-        empty=false
+        break
     fi
 done
 
--- compare.py
+++ compare.py
@@ -84,6 +84,7 @@ def finalCheck(TName,skipCksm):
     bashScript.write('#! /bin/bash \n\n')
     bashScript.write('# This script will clear your site of files that the Consistency Check identified \n')
     bashScript.write('# as not being tracked by PhEDEx. i.e. PhEDEx does not know they are here. \n\n')
+    clearList = []
     for aBlock in secondData:
         aDirectory = aBlock['directory']
         bDirectoryList = []
@@ -103,7 +104,9 @@ def finalCheck(TName,skipCksm):
                     report.write(aDirectory + aName + ' \n')
                     bashScript.write('rm ' + aDirectory + aName + ' \n')
         else:
+            clearList.append(aDirectory)
             report.write('PhEDEx expects nothing in ' + aDirectory + ' \n')
+            bashScript.write('cd ' + aDirectory + ' \n')
             bashScript.write('bash clearDirectory.sh ' + aDirectory + ' \n')
     report.write('\n')
     report.close()
