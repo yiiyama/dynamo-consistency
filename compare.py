013ecc7c711909e896fecc6a9c6614e432413478
dabercro <daniel.r.abercrombie@gmail.com>
2014-12-03 14:20:55 -0500

    Removed output from not searching directory

--- compare.py
+++ compare.py
@@ -98,13 +98,11 @@ def finalCheck(TName,skipCksm):
             wasSearched = True                                                 # First check that the directory is one that was searched by ConsistencyCheck
             if os.path.exists(aDirectory):                                     # If there's a directory there, then there might be no problem
                 wasSearched = False                                            # It just wasn't checked
-            writeBlock(aBlock['dataset'],report)                               # Either way, note the block name
             if wasSearched:
+                writeBlock(aBlock['dataset'],report)                           # Note the block name
                 report.write('No files were found in ' + aDirectory + ' \n')   # If there is no directory where there should be, this might be a problem
                 for aFile in aBlock['files']:                                  # List the files that should be included
                     report.write(aDirectory + aFile['file'] + ' \n')            
-            else:
-                report.write('Did not search ' + aDirectory + ' \n')           # Otherwise, just state that the directory had not been searched
 
     print '*********************************************'
     print ' Checking if all present files are in PhEDEx '
