71238c34a5a573809a8454f32eb7fffb33efcb95
dabercro <daniel.r.abercrombie@gmail.com>
2015-02-18 17:38:01 -0500

    This script has changed enough to merit an update.

--- crab/update.sh
+++ crab/update.sh
@@ -5,13 +5,15 @@ TEST=$1
 # This is a nice little script that submits jobs for me, cleans up my space, and updates the website
 
 # This is where I am keeping all of the output:
-STOREDIR='/scratch/dabercro/ConsistencyCheck/crabRuns'
+STOREDIR='/scratch/dabercro/ConsistencyCheck/runs'
 FAILDIR='/home/dabercro/ConsistencyCheck/FailedCheck'
 SERVERDIR='/home/cmsprod/public_html/ConsistencyChecks'
+CKSMDIR='/home/dabercro/ConsistencyCheck/CheckSums/Cache'
 
-# "T2_US_Nebraska" is not working, because they are naughty. i.e. not my fault, apparently.
-# "T2_FR_CCIN2P3" also seems to have an incorrect TFC
-declare -a SITES=("T1_US_FNAL_Disk" "T1_US_FNAL_MSS" "T2_US_Caltech" "T2_US_Florida" "T2_US_MIT" "T2_US_Purdue" "T2_US_UCSD" "T2_US_Vanderbilt" "T2_US_Wisconsin")
+#declare -a SITES=("T1_US_FNAL_Disk")
+# "T1_US_FNAL_MSS")
+#declare -a SITES=("T2_AT_Vienna" "T2_BE_IIHE" "T2_BE_UCL" "T2_BR_SPRACE" "T2_BR_UERJ" "T2_CH_CERN" "T2_CH_CSCS" "T2_CN_Beijing" "T2_DE_DESY" "T2_DE_RWTH" "T2_EE_Estonia" "T2_ES_CIEMAT" "T2_ES_IFCA" "T2_FI_HIP" "T2_FR_CCIN2P3" "T2_FR_GRIF_IRFU" "T2_FR_GRIF_LLR" "T2_FR_IPHC" "T2_GR_Ioannina" "T2_HU_Budapest" "T2_IN_TIFR" "T2_IT_Bari" "T2_IT_Legnaro" "T2_IT_Pisa" "T2_IT_Rome" "T2_KR_KNU" "T2_MY_UPM_BIRUNI" "T2_PK_NCP" "T2_PL_Swierk" "T2_PL_Warsaw" "T2_PT_NCG_Lisbon" "T2_RU_IHEP" "T2_RU_INR" "T2_RU_ITEP" "T2_RU_JINR" "T2_RU_PNPI" "T2_RU_RRC_KI" "T2_RU_SINP" "T2_TH_CUNSTDA" "T2_TR_METU" "T2_UA_KIPT" "T2_UK_London_Brunel" "T2_UK_London_IC" "T2_UK_SGrid_Bristol" "T2_UK_SGrid_RALPP" "T2_US_Caltech" "T2_US_Florida" "T2_US_MIT" "T2_US_Nebraska" "T2_US_Purdue" "T2_US_UCSD" "T2_US_Vanderbilt" "T2_US_Wisconsin")
+declare -a SITES=("T2_BE_IIHE" "T2_ES_CIEMAT" "T2_IT_Pisa" "T2_IT_Rome" "T2_US_Caltech" "T2_US_Florida" "T2_US_MIT" "T2_US_Nebraska" "T2_US_Purdue" "T2_US_UCSD" "T2_US_Vanderbilt" "T2_US_Wisconsin")
 
 for SITE in "${SITES[@]}";do
     COUNTDIR=`ls -d $SITE-* | wc -l`
@@ -31,8 +33,14 @@ for SITE in "${SITES[@]}";do
                 echo "Making a new folder!"                      # If not there, then make it
                 mkdir $SERVERDIR/$SITE
             fi
-            cp $SITE*results.txt $SERVERDIR/$SITE/.              # Put results on the server
-            cp $SITE*summary.txt $SERVERDIR/$SITE/.              # Put summary on server to be read
+            cp $SITE*.txt $SERVERDIR/$SITE/.                     # Put results on the server
+            if [ ! -d "$CKSMDIR/$SITE" ]; then                  # Make sure the right folder is there
+                echo "Preparing a checksum cache for $SITE."
+                echo "Making a new folder!"                      # If not there, then make it
+                mkdir $CKSMDIR/$SITE
+            fi
+            cp $SITE\_phedex.json $CKSMDIR/$SITE/.
+            cp $SITE\_skipCksm_exists.json $CKSMDIR/$SITE/.
             rm $SITE*.json $SITE*.txt                            # Clean up the stuff from tar
             cd -                                                 # Now cd back
             mv $SITE-* $STOREDIR/.                               # Now store that stuff
