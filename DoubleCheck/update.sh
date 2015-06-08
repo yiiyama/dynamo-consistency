aad234e2f16b6cb69ddd64ffc0cf41f63579485e
dabercro <daniel.r.abercrombie@gmail.com>
2015-06-08 05:44:19 -0400

    Adding update mechanism for double check.

---
+++ DoubleCheck/update.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+wget http://t3serv001.mit.edu/~cmsprod/ConsistencyChecks/T2_US_MIT/T2_US_MIT_skipCksm_missing.txt
+wget http://t3serv001.mit.edu/~cmsprod/ConsistencyChecks/T2_US_MIT/T2_US_MIT_skipCksm_removable.txt
