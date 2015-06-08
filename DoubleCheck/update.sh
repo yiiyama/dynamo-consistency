6c2cdd96df3692274e05d0cf2fb2b12c43b1dc56
dabercro <daniel.r.abercrombie@gmail.com>
2015-06-08 07:26:41 -0400

    Clean up before each download.

--- DoubleCheck/update.sh
+++ DoubleCheck/update.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
+rm T2_US_MIT*
+
 wget http://t3serv001.mit.edu/~cmsprod/ConsistencyChecks/T2_US_MIT/T2_US_MIT_skipCksm_missing.txt
 wget http://t3serv001.mit.edu/~cmsprod/ConsistencyChecks/T2_US_MIT/T2_US_MIT_skipCksm_removable.txt
