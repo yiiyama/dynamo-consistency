e9bf819e31969eb7adba592cf15b2c05782e0fc7
dabercro <daniel.r.abercrombie@gmail.com>
2016-02-29 15:39:56 -0500

    Format of something changed. Also don't need to check for duplicates.

--- Cache/mergeFiles.py
+++ Cache/mergeFiles.py
@@ -33,7 +33,6 @@ if not opts.__dict__['TName']:
 
 TName = opts.TName                                              # Name of the site is stored here
 
-# Next move on to the files that we should search for. Ignore deleted blocks and duplicates.
 if not os.path.exists(TName+'/'+TName+'_lfn2pfn.json'):
     print 'Getting TFC...'
     os.system('wget --no-check-certificate -O '+TName+'/'+TName+'_lfn2pfn.json https://cmsweb.cern.ch/phedex/datasvc/json/prod/lfn2pfn?node='+TName+'\&protocol=direct\&lfn=/store/data/test.root')
@@ -49,17 +48,12 @@ except:
     print 'Exiting...'
     exit()
 
-duplicateList = []
-
 blockList = []
 for block in inData:
-    if str(block['dataset']) in duplicateList:
-        continue
-    duplicateList.append(str(block['dataset']))
     if block['directory'].startswith(prefix):
         blockList.append({'directory':str(block['directory']),'files':block['files'],'dataset':str(block['dataset'])})
     else:
-        blockList.append({'directory':prefix+"/"+str(block['directory'])+"/",'files':block['files'],'dataset':str(block['dataset'])})
+        blockList.append({'directory':prefix+str(block['directory'])+"/",'files':block['files'],'dataset':str(block['dataset'])})
 
 outfile = open(TName+'/'+TName+'_phedex.json','w')
 outfile.write(json.dumps(blockList))
