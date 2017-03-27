afd736d919b3a18ec70e8646596dcaab01c3e8c6
dabercro <daniel.r.abercrombie@gmail.com>
2017-03-27 17:15:17 -0400

    Added a test script for Brian to time.

---
+++ test/single_site_list.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+# Simply lists a single site and times it
+
+import logging
+import time
+
+from ConsistencyCheck import getsitecontents
+
+# Set this however you'd like
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+
+if __name__ == '__main__':
+    start = time.time()
+    tree = getsitecontents.get_site_tree('T2_US_Nebraska')
+    print '\nDuration: %f seconds\n' % (time.time() - start)
