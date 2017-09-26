9b78e3b2c40e6e811eaf2cd5bd80e218e6f65fee
dabercro <daniel.r.abercrombie@gmail.com>
2017-09-25 21:55:03 -0400

    Add header to all non-production web pages.

--- web/install.sh
+++ web/install.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
 TARGET=$1
-DEFAULT=$HOME/public_html/ConsistencyCheck
+
+if [ "$USER" = "dynamo" ]
+then
+    DEFAULT=/var/html/www/dynamo/consistency
+else
+    DEFAULT=$HOME/public_html/ConsistencyCheck
+fi
 
 if [ -z $TARGET ]
 then
@@ -25,5 +31,10 @@ fi
 test -d $TARGET || mkdir -p $TARGET
 
 cp output.html stats.php stylin.css explanations.html $TARGET
+
+if [ "$USER" != "dynamo" ]
+then
+    sed -i 's@<body>@<body> <p style="color:#ff0000;font-weight:bold;">Note: As we finish commissioning of US T2 sites, this is transistioning into a development/test instance of the webpage. See the production summary <a href="http://dynamo.mit.edu/consistency/">here</a>.</p>@' $TARGET/output.html
+fi
 
 exit $?
