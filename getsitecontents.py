aea83ce8f6c18e93900be4115c2a4f532c91ce72
dabercro <daniel.r.abercrombie@gmail.com>
2017-10-20 22:33:12 -0400

    Pylint recommends cleaning up bool logic.

--- getsitecontents.py
+++ getsitecontents.py
@@ -111,7 +111,7 @@ class XRootDLister(object):
             # Retry certain error codes if there's no dir_list
             # Don't bother with 3010 at the moment because there's that Hadoop bug
             okay = (error_code == '3010' and 'operation not permitted' in status.message) or \
-                (bool(dir_list) if error_code in ['!', '3005'] else False)
+                (bool(dir_list) and error_code in ['!', '3005'])
 
             self.log.debug('Error code: %s', error_code)
             self.log.debug('Directory List: %s', dir_list)
