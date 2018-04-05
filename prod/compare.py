5428ec7ed8b055f4c1e7ba438f90533f01f8b078
Dynamo <dynamo@dynamo.mit.edu>
2018-04-05 14:50:41 -0400

    Count the removed nodes in the final count.

--- prod/compare.py
+++ prod/compare.py
@@ -516,7 +516,8 @@ def main(site):
             REPLACE INTO stats VALUES
             (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, DATETIME(DATETIME(), "-{0} hours"), ?, ?)
             """.format(5 - is_dst),
-            (site, time.time() - start, site_tree.get_num_files(), site_tree.count_nodes(),
+            (site, time.time() - start, site_tree.get_num_files(),
+             remover.get_removed_count() + site_tree.count_nodes(),
              remover.get_removed_count() + len(site_tree.empty_nodes_list()),
              config_dict.get('NumThreads', config_dict.get('MinThreads', 0)),
              len(missing), m_size, len(orphan), o_size, len(no_source_files),
